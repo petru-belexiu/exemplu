36872557562aa89c1fa8335a57fa5f4a940d4912
Petru Belexiu <petru.belexiu@outlook.com>
2017-06-27 14:26:01 +0300

    say "bye" to the name read from name.txt

--- OnMaster/HelloWorld/Program.cs
+++ OnMaster/HelloWorld/Program.cs
@@ -7,7 +7,7 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             string name = System.IO.File.ReadAllText("name.txt");
-            Console.WriteLine("See you later {0}", name);
+            Console.WriteLine("Bye {0}", name);
             Console.ReadKey();
         }
     }
